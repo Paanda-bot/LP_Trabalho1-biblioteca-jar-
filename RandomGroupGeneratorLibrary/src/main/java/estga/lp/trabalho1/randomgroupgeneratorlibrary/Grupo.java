3ea2013c769c9b17f8af2470c1358451929b6a5f
awolf23 <antoniolobo83@gmail.com>
2026-02-28 15:19:30 +0000

    Implementa construtor da Classe Grupo c/ validação

--- RandomGroupGeneratorLibrary/src/main/java/estga/lp/trabalho1/randomgroupgeneratorlibrary/Grupo.java
+++ RandomGroupGeneratorLibrary/src/main/java/estga/lp/trabalho1/randomgroupgeneratorlibrary/Grupo.java
@@ -6,10 +6,21 @@ public class Grupo {
     private final Estudante e2;
 
     public Grupo(Estudante e1, Estudante e2) {
+        //condição para controlar estudante null
         if (e1 == null || e2 == null) {
-           throw new IllegalArgumentException ("Grupo inválido: Estudante null. ")
+            throw new IllegalArgumentException("Grupo inválido: Estudante null.");
         }
-    }   this.e1 = e1;
+
+        //condição para controlo de estudantes repetidos
+        if (e1.getNumero() == e2.getNumero()) {
+            throw new IllegalArgumentException("Grupo inválido: Estudantes repetidos");
+        }
+
+        this.e1 = e1;
         this.e2 = e2;
 
+    }
+
 }
+
+
