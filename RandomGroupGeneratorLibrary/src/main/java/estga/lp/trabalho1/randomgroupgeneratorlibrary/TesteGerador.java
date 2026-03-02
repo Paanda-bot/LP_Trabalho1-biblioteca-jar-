18ccec6abb3d637e49d6bdd43f1e86269f482441
awolf23 <antoniolobo83@gmail.com>
2026-03-02 19:37:11 +0000

    Implementado Main Test

--- RandomGroupGeneratorLibrary/src/main/java/estga/lp/trabalho1/randomgroupgeneratorlibrary/TesteGerador.java
+++ RandomGroupGeneratorLibrary/src/main/java/estga/lp/trabalho1/randomgroupgeneratorlibrary/TesteGerador.java
@@ -1,4 +1,34 @@
 package estga.lp.trabalho1.randomgroupgeneratorlibrary;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class TesteGerador {
+
+    public static void main(String[] args) {
+
+        Collection<Estudante> estudantes = new ArrayList<>();
+
+        estudantes.add(new Estudante("Ana", 131415));
+        estudantes.add(new Estudante("Manel", 151413));
+        estudantes.add(new Estudante("Joaquim", 111416));
+        estudantes.add(new Estudante("Ana", 121211));
+        estudantes.add(new Estudante("Maria", 107817));
+        estudantes.add(new Estudante("Zé", 131418));
+        estudantes.add(new Estudante("Rita", 191418));
+
+
+        GeradorDeGrupos gerador = new GeradorDeGrupos(estudantes);
+
+        Collection<Grupo> grupos = gerador.gerarGrupos();
+
+        for (Grupo g : grupos) {
+            System.out.println(
+                    "Grupo " + g.getNumeroGrupo() + ": "
+                            + g.getE1().getNome() + " [" + g.getE1().getNumero() + "]" + " / " +
+                            g.getE2().getNome() + " [" + g.getE2().getNumero() + "]"
+            );
+        }
+        System.out.println("Sem grupo: " + gerador.getEstudanteSemGrupo().getNome() + " [" + gerador.getEstudanteSemGrupo().getNumero() + "]" );
+    }
 }
